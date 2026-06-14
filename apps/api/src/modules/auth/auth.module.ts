b8275754256672aa79099e9acf09b56c579745e2
Owen Steger Porter <oporter8@ivytech.edu>
2026-06-14 15:32:14 -0400

    Create AuthModule with JWT integration

---
+++ apps/api/src/modules/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET || 'development-secret',
+      signOptions: {
+        expiresIn: '7d',
+      },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
+})
+export class AuthModule {}
