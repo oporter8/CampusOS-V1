3fae8017f28484a9c57d45f6b03ebf7be6b56312
Owen Steger Porter <oporter8@ivytech.edu>
2026-06-14 15:36:08 -0400

    Add AuthModule to AppModule imports

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { AuthModule } from './modules/auth/auth.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+
+    AuthModule,
   ],
 })
 export class AppModule {}
