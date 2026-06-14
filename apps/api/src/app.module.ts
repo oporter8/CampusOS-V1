689b7e2cca3123c8b4d0ab655487811bae42b3dc
Owen Steger Porter <oporter8@ivytech.edu>
2026-06-14 15:51:55 -0400

    Add PrismaModule to AppModule imports

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { PrismaModule } from './database/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { HealthController } from './modules/health/health.controller';
 
@@ -10,6 +11,7 @@ import { HealthController } from './modules/health/health.controller';
       isGlobal: true,
     }),
 
+    PrismaModule,
     AuthModule,
   ],
   controllers: [HealthController],
