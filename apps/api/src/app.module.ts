a724e90730344815b80cd61b09b787e98d300a88
Owen Steger Porter <oporter8@ivytech.edu>
2026-06-14 15:55:29 -0400

    Add UsersModule to AppModule imports

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,7 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { PrismaModule } from './database/prisma.module';
+
 import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+
 import { HealthController } from './modules/health/health.controller';
 
 @Module({
@@ -12,6 +15,9 @@ import { HealthController } from './modules/health/health.controller';
     }),
 
     PrismaModule,
+
+    UsersModule,
+
     AuthModule,
   ],
   controllers: [HealthController],
